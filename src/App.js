8e07d76bdb307ccc8a5f376b52a01e026a9a243f
yyagoub <imjou9@gmail.com>
2020-05-20 20:23:51 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -6,8 +6,25 @@ import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div class='grid-2'>
-      <Sidebar />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+
+      <div class='grid-2'>
+        <Sidebar />
+      </div>
     </div>
   );
 }
