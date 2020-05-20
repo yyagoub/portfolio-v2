1baa8f81e12ccc700395faa20580574bfd214bf7
yyagoub <imjou9@gmail.com>
2020-05-20 20:22:42 +0300

    deploy

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div class='section-1'>
+      <i class='fas fa-code fa-5x white'></i>
+      <h2>Yousef Yagoub,</h2>
+      <p>Software Developer.</p>
+    </div>
+  );
+}
+
+export default Sidebar;
