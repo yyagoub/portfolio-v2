609c423a429fff1e53b92e3152a7487f3fb30541
yyagoub <imjou9@gmail.com>
2020-05-20 20:28:53 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import './style.css';
 import Sidebar from './components/Sidebar';
+import About from './pages/About';
 
 function App() {
   return (
     <div class='grid-2'>
       <Sidebar />
+      <About />
     </div>
   );
 }
---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        <code>
+          I’m a software developer, lucky enough to love what I do for living,
+          specialist in server-side applications and did some client-side
+          applications using React. Passionate for new technologies, and always
+          trying to implement and document what I've learned.
+        </code>
+      </p>
+      <p>
+        <code>
+          I feel myself in a constant search for pragmatic solutions. Not one
+          for tradition, striving for innovation, while helping those around me
+          to with product requirements.
+        </code>
+      </p>
+      <p>
+        <code>
+          Tools and Technologies: Java, JavaScript, Git, Maven, Npm, SQL, Spring
+          Boot Frameworks (Web, Data, Security, ..), React.js (Redux, Axios,
+          Router..)
+        </code>
+      </p>
+    </div>
+  );
+}
