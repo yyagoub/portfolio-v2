bd32c16f7d25a039b37a986652faffa748b412bf
yyagoub <imjou9@gmail.com>
2020-05-20 20:14:15 +0300

    Updates

---
+++ precache-manifest.0da086b724c239bf097a131e32148373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e7f9bb2e57853509a633b31ab601b4",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "59f8b3d21b827c9f2ee9",
+    "url": "/portfolio-v2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "245a1fadf0adcc3db67c",
+    "url": "/portfolio-v2/static/js/2.4cb33f94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-v2/static/js/2.4cb33f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f8b3d21b827c9f2ee9",
+    "url": "/portfolio-v2/static/js/main.6c896789.chunk.js"
+  },
+  {
+    "revision": "030e376544c5108b22f3",
+    "url": "/portfolio-v2/static/js/runtime-main.0d9bf426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-v2/static/media/logo.5d5d9eef.svg"
+  }
+]);
